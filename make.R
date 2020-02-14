47d0099d577fc36929d381f99e0e71d0df38afb4
Peder <pbac@dtu.dk>
2020-02-14 20:21:48 +0100

    Now first version

--- make.R
+++ make.R
@@ -1,44 +1,46 @@
 library(rmarkdown)
 
-## Compile
+## Compile main pages
 render("index.Rmd")
-render("sv_index.Rmd")
-render("da_index.Rmd")
 render("manual.Rmd")
 render("about.Rmd")
 
 
-
 ## The pages to be included, just find all ".Rmd" files containing with "teachers" or "students"
-pages <- dir(pattern="teachers.*\\.Rmd$|students.*\\.Rmd$")
-## Or specify directly
-## pages <- c("teachers_sv.Rmd","students_sv.Rmd",
-##            "teachers_da.Rmd","students_da.Rmd")
-## Compile the joined pages
-for(page in pages){
-    render(page, envir=new.env())
-}
-
-## Compile all the instructions to pdfs
-for(page in pages){
-    x <- scan(page, what="character")
-    pattern <- ".*knitr\\:\\:knit_child\\('"
-    x <- x[grep(pattern, x)]
-    files <- sub("').*", "", sub(pattern, "", x))
-    for(file in files){
-        ## Move the template setup files
-        file.copy("website/pdf_output.yaml", paste0(dirname(file),"/_output.yaml"), overwrite=TRUE)
-        file.copy("website/pdf_header.tex", paste0(dirname(file),"/_header.tex"), overwrite=TRUE)
-        ## Fix the path to the header image files
-        tmpfile <- paste0(dirname(file),"/_header.tex")
-        x <- scan(tmpfile, what="character", sep="\n")
-        x <- gsub("figures_pdf", paste0(paste0(rep("..", lengths(regmatches(file, gregexpr("/",file)))), collapse="/"), "/website/figures_pdf"), x)
-        write.table(x, tmpfile, row.names=FALSE, col.names=FALSE, quote=FALSE)
-        ## Compile the pdf file
-        render(file, output_format="pdf_document", envir=new.env())
+langs <- c("sv","da")
+pages <- character()
+for(lang in langs){
+    ## ##
+    pages <- dir(lang, pattern=paste0("\\.Rmd$"), full.names=TRUE)
+    ## Copy the yaml
+    file.copy("website/html_output.yaml", paste0(lang,"/_output.yaml"), overwrite=TRUE)
+    ## ##
+    ## Compile the joined pages
+    for(page in pages){
+        render(page, envir=new.env())
+    }
+    ## ##
+    ## Compile all the instructions to pdfs
+    for(page in pages){
+        x <- scan(page, what="character", sep="\n")
+        pattern <- ".*knitr\\:\\:knit_child\\('"
+        x <- x[grep(pattern, x)]
+        files <- sub("').*", "", sub(pattern, "", x))
+        for(file in files){
+            file <- paste0(lang,"/",file)
+            ## Move the template setup files
+            file.copy("website/pdf_output.yaml", paste0(dirname(file),"/_output.yaml"), overwrite=TRUE)
+            file.copy("website/pdf_header.tex", paste0(dirname(file),"/_header.tex"), overwrite=TRUE)
+            ## Fix the path to the header image files
+            tmpfile <- paste0(dirname(file),"/_header.tex")
+            x <- scan(tmpfile, what="character", sep="\n")
+            x <- gsub("figures_pdf", paste0(paste0(rep("..", lengths(regmatches(file, gregexpr("/",file)))), collapse="/"), "/website/figures_pdf"), x)
+            write.table(x, tmpfile, row.names=FALSE, col.names=FALSE, quote=FALSE)
+            ## Compile the pdf file
+            render(file, output_format="pdf_document", envir=new.env())
+        }
     }
 }
-
 
 ## Publish
 ## system("git add -A")
